50e6c5582bd9f26e0b471b1140cbb31667b20483
Mary <omota4fidelity@gmail.com>
2023-10-08 13:11:29 +0100

    updated

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count ):
+        self.title = title
+        self.page_count = page_count
+        pass
     
-        
+    @property
+    def get_page_count(self):
+        return self.page_count
+    
+    def set_page_count(self, page_count):
+        if (type(page_count) in (int, float)):
+            return page_count
+        else:
+            print("page_count must be an integer\n.")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+           
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def get_size(self):
+        return self.size
+    
+    def set_size(self, size):
+        if (type(size) in (int, float)):
+            self.size = size
+        else:
+            print("size must be an integer\n")
+        
+    def cobble(self):
+        self.condition = "new"
+        print("Your shoe is as good as new!")
+
+    
